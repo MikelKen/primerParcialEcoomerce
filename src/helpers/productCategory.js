2d3dd7baa36d7b0b87579afc3fdabcb37f845364
MikelKen <164554620+MikelKen@users.noreply.github.com>
2024-09-30 15:00:47 -0400

    Aun

--- src/helpers/productCategory.js
+++ src/helpers/productCategory.js
@@ -1,16 +1,18 @@
 const productCategory = [
-    { id : 1, label : "Clothes", value : "clothes" },
-    { id : 2, label : "Pants and Shorts", value : "pantsShorts" },
-    { id : 3, label : "Jackets", value : "jackets" },
-    { id : 4, label : "Blazers", value : "blazers" },
-    { id : 5, label : "T-Shirts", value : "t-shirts" },
-    { id : 6, label : "Tops", value : "tops" },
-    { id : 7, label : "Shirts and Blouses", value : "shirtsBlouses" },
-    { id : 8, label : "Skirts", value : "skirts" },
-    { id : 9, label : "Sweaters", value : "sweaters" },
-    { id : 10, label : "Jeans", value : "jeans" },
-    { id : 11, label : "Coats", value : "coats" },
-    { id : 12, label : "Boots", value : "boots" }
+    { id : 1, label : "Blazer", value : "blazer" },
+    { id : 2, label : "Boots", value : "boots" },
+    { id : 3, label : "Coats", value : "coats" },
+    { id : 4, label : "Jackets", value : "jackets" },
+    { id : 5, label : "Jeans", value : "jeans" },
+    { id : 6, label : "Jerseis", value : "jerseis" },
+    { id : 7, label : "Pants", value : "pants" },
+    { id : 8, label : "Shirts", value : "shirts" },
+    { id : 9, label : "Shorts", value : "shorts" },
+    { id : 10, label : "Skirts", value : "skirts" },
+    { id : 11, label : "Tops", value : "tops" },
+    { id : 12, label : "Wads", value : "wads" },
+    { id : 13, label : "Zendas", value : "zendas" },
+    { id : 14, label : "Caps", value : "caps" },
 
 ]
 
